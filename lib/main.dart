b0fc21e90c90581ae65b0ae4b031321d635e9a8d
Shirinboyev <gayratshirinboyev6707@gmail.com>
2022-12-14 17:17:17 +0500

    4

--- lib/main.dart
+++ lib/main.dart
@@ -13,85 +13,105 @@ class Mywidget extends StatefulWidget {
   State<Mywidget> createState() => _MywidgetState();
 }
 
-int scole = 0;
+int scole = 1;
 int index = 0;
 List path = [
+  SingleChildScrollView(
+    child: Column(
+      children: [
+        Image.asset('images/2.png'),
+        Text('Iphone 2', style: TextStyle(fontSize: 30))
+      ],
+    ),
+  ),
   Column(
     children: [
-      Image.asset('images/oy.png'),
-      Text(
-        '\n\nOY',
-        style: TextStyle(fontSize: 30),
-      )
+      Image.asset('images/3.png'),
+      Text('Iphone 3', style: TextStyle(fontSize: 30))
     ],
   ),
   Column(
     children: [
-      Image.asset('images/dar.png'),
-      Text('data', style: TextStyle(fontSize: 30))
+      Image.asset('images/4.png'),
+      Text('Iphone 4', style: TextStyle(fontSize: 30))
     ],
   ),
   Column(
     children: [
-      Image.asset('images/nat.png'),
-      Text('data', style: TextStyle(fontSize: 30))
+      Image.asset('images/5.png'),
+      Text('Iphone 5', style: TextStyle(fontSize: 30))
     ],
   ),
   Column(
     children: [
-      Image.asset('images/img.png'),
-      Text('data', style: TextStyle(fontSize: 30))
+      Image.asset('images/5s.png'),
+      Text('Iphone 5s', style: TextStyle(fontSize: 30))
     ],
   ),
   Column(
     children: [
-      Image.asset('images/yusuf.png'),
-      Text('data', style: TextStyle(fontSize: 30))
+      Image.asset('images/6.png'),
+      Text('Iphone 6', style: TextStyle(fontSize: 30))
     ],
-  )
+  ),
+  Column(
+    children: [
+      Image.asset('images/6s.png'),
+      Text('Iphone 6s', style: TextStyle(fontSize: 30))
+    ],
+  ),
+  Column(
+    children: [
+      Image.asset('images/7.png'),
+      Text('Iphone 7', style: TextStyle(fontSize: 30)),
+    ],
+  ),
 ];
 
 class _MywidgetState extends State<Mywidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // if (scole > 4)
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              // Image.asset(path[index %4]),
-              path[index],
-              Padding(
-                padding: const EdgeInsets.only(top: 200),
-                child: Container(
-                  // alignment: Alignment.bottomCenter,
-                  color: Colors.white,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      TextButton(
-                          onPressed: () {
-                            setState(() {
-                              if (index > 0) {
-                                index--;
-                                scole--;
-                              }
-                              if (scole == index.bitLength) ;
-                            });
-                          },
-                          child: Icon(
-                            Icons.arrow_back,
-                            size: 40,
-                          )),
-                      Text(
-                        'Score $scole',
-                        style: TextStyle(fontSize: 30),
-                      ),
-                      TextButton(
+      appBar: AppBar(title: Text('Iphone page'),),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // if (scole > 4)
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                // Image.asset(path[index %4]),
+                path[index],
+                Padding(
+                  padding: const EdgeInsets.only(top: 200),
+                  child: Container(
+                    // alignment: Alignment.bottomCenter,
+                    color: Colors.white,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        TextButton(
+                            onPressed: () {
+                              setState(() {
+                                if (index > 0) {
+                                  index--;
+                                  scole--;
+                                }
+                                if (scole == index.bitLength) ;
+                              });
+                            },
+                            child: Icon(
+                              Icons.arrow_back,
+                              size: 40,
+                            )),
+                        Text(
+                          'Score $scole',
+                          style: TextStyle(fontSize: 30),
+                        ),
+                        TextButton(
                           onPressed: () {
                             setState(() {
                               if (index != path.length - 1) {
@@ -103,14 +123,16 @@ class _MywidgetState extends State<Mywidget> {
                           child: Icon(
                             Icons.arrow_forward,
                             size: 40,
-                          ))
-                    ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
